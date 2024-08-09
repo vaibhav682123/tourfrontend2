77476324aa56d77abb4e272f4f29a4aa8c678fff
vaibhav682123 <khandelwalhitesh1978@gmail.com>
2024-08-09 16:27:23 +0530

    false to true

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 import { FiShoppingCart, FiLogIn } from "react-icons/fi";
 import { FaUser } from "react-icons/fa";
 import {motion} from "framer-motion"
-const Header = ({ isAuthenticated =false }) => {
+const Header = ({ isAuthenticated  }) => {
   return (
     <nav>
       <motion.div
